7ed94e2525fb9bc8261660165f80079064b616e0
liyuqihxc <392036904@qq.com>
2019-11-06 13:54:52 +0800

    zuul

--- java-services/eureka-server/src/main/java/com/liyuqihxc/iwm/eurekaserver/EurekaServerApplication.java
+++ java-services/eureka-server/src/main/java/com/liyuqihxc/iwm/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.liyuqihxc.eurekaserver;
+package com.liyuqihxc.iwm.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- java-services/zuul-server/src/main/java/com/liyuqihxc/iwm/zuulserver/ServletInitializer.java
+++ java-services/zuul-server/src/main/java/com/liyuqihxc/iwm/zuulserver/ServletInitializer.java
@@ -1,13 +1,13 @@
-package com.liyuqihxc.eurekaserver;
+package com.liyuqihxc.iwm.zuulserver;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(EurekaServerApplication.class);
+		return application.sources(ZuulServerApplication.class);
 	}
 	
 }
